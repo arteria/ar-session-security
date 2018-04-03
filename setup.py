f7525efc1525cc46d8bda6c068bfd3d11b4371a1
Philippe O. Wagner <wagner@arteria.ch>
2018-04-03 15:42:23 +0200

    .

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ if 'sdist' in sys.argv:
 
 setup(
     name='ar-session-security',
-    version='2.6.0',
+    version='2.6.1',
     description='Let the user secure his session for usage in public computers',
     author='James Pic',
     author_email='jamespic@gmail.com',
@@ -71,4 +71,3 @@ setup(
         'Topic :: Software Development :: Libraries :: Python Modules',
     ]
 )
-
